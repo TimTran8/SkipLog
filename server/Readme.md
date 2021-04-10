66792745902b0dfbf643fde86eef365482388ab2
tht7 <tim_tran@sfu.ca>
2021-04-09 21:56:10 -0700

    Added readme

---
+++ server/Readme.md
@@ -0,0 +1,22 @@
+# Skiplog
+* Track skipping workouts
+* Create accounts
+
+*Still a work in progress
+
+## How to Run
+```
+# Run the following command in the client directory
+$ npm install
+$ npm start
+# Run the following command in the server directory 
+$ nodemon app
+```
+
+## Technologies Used
+- ReactJS with bootstrap for frontend
+- MongoDB and Mongoose for database
+- React Axios library for HTTP requests
+- NodeJS and Express for backend
+- Bcrypt and JWT for authentication and authorization
+- Mocha and Chai for unit tests
--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -2,12 +2,18 @@ const router = require('express').Router();
 const Workout = require('../models/workout.model');
 let User = require('../models/workout.model');
 
-router.route('/getWorkouts/:userId').get((req, res) => {
+router.route('/getWorkouts/:id').get((req, res) => {
 // router.route('/getWorkouts/').get((req, res) => {
   console.log("", req.params);
-    Workout.find()
-        .then(workouts => res.status(200).json(workouts))
-        .catch(err => res.status(400).json('Error: ' + err));
+  Workout.findById(req.params.id)
+    .then(workouts => {
+      if (!workouts) { return res.status(404).end() };
+      return res.status(200).json(user);
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+    // Workout.find()
+    //     .then(workouts => res.status(200).json(workouts))
+    //     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
